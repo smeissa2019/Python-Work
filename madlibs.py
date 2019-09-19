3dc9355312785897c3833ed2865b9eb4c2260f76
Sahar Eissa <sa418774@ucf.edu>
2019-09-18 20:45:41 -0400

    go

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+
+#%%
+import random
+y = 1
+z=[]
+verbs = ["Jump","plays","run"]
+adj =["beautiful", "amazing","pretty"]
+
+while y >= 1:
+   if len(z) == 0 :
+      sentence = print("yes " + random.choice(adj) + " cats " + random.choice(verbs))
+sentence.append(z)  
+else:
+   len(z) == 10:
+   sentence = print("STOP")
+sentence.append(z)     
+exit()
+print(z)
+
---
+++ my try.py
@@ -0,0 +1,5 @@
+x = []
+def User(keyword):
+new_keyword = input("Please enter your keyword: ")
+x.append(new_keyword [-10])
+    print(x)
