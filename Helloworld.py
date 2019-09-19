ab60e30d1ced82d7526666f5311fe8d0f92b0646
Sahar Eissa <sa418774@ucf.edu>
2019-09-18 20:47:34 -0400

    go1

--- Helloworld.py
+++ Helloworld.py
@@ -1,5 +1,20 @@
-print ("Hello World")
-10 * 3
+import random
+y = 1
+z=[]
+verbs = ["Jump","plays","run"]
+adj =["beautiful", "amazing","pretty"]
+
+while y >= 1:
+   if len(z) == 0 :
+      sentence = print("yes " + random.choice(adj) + " cats " + random.choice(verbs))
+sentence.append(z)  
+else:
+   len(z) == 10:
+   sentence = print("STOP")
+sentence.append(z)     
+exit()
+print(z)
+
 
 
 
