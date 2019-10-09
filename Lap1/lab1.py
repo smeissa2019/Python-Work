bb9ee28557232c316beabf11447561647b6730fa
smeissa2019 <sa418774@ucf.edu>
2019-10-09 09:22:08 -0400

    final - final - correct

--- Lap1/lab1.py
+++ Lap1/lab1.py
@@ -385,12 +385,18 @@ def sum_random(y):
         x = random.randint(0,110) 
         for i in range(x):
             if i < int(y):
-                z = x + 1
-                w.append(z)
+                 z = x
+                 
+                 x = random.randint(0,110)
+                 w.append(z)
             if i ==y:
                 break
+        print(w)
         sahar = sum(w)
         print(sahar)
 
 sum_random(6)
 
+
+
+#%%
