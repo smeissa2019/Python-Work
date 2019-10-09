7cdae3bba874110a6418d60dded791631d59c01b
smeissa2019 <sa418774@ucf.edu>
2019-10-09 09:40:43 -0400

    df

--- Lap1/lab1.py
+++ Lap1/lab1.py
@@ -395,8 +395,28 @@ def sum_random(y):
         sahar = sum(w)
         print(sahar)
 
-sum_random(6)
+sum_random(3)
 
 
+#%%
+import random
+w = []
+sahar = []        
+
+def sum_random(y):
+        x = random.randint(0,110) 
+        for i in range(x):
+            if i < int(y):
+                 z = x
+                 
+                 x = random.randint(0,110)
+                 w.append(z)
+            if i ==y:
+                break
+        print(w)
+        sahar = sum(w)
+        print(sahar)
+
+sum_random(100)
 
 #%%
